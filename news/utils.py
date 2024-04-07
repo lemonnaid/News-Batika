9ba1bb910bf5c4100454171e4d5385397ef98c34
lalitasapkota <lalitasapkota09@gmail.com>
2024-04-07 15:20:58 +0545

    Template for cosine match

--- news/utils.py
+++ news/utils.py
@@ -1,22 +1,43 @@
+from sklearn.preprocessing import normalize
+from sklearn.decomposition import NMF
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 from .models import Headline
 
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.corpus import stopwords
+from sklearn.decomposition import NMF
+from sklearn.preprocessing import normalize
+from sklearn.metrics.pairwise import cosine_similarity
+from .models import Headline
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS as sklearn_stop_words
+
 def get_similar_news(news_id):
+    # Input and output data
     articles = Headline.objects.all()
-    article_titles = [article.title for article in articles]
+    article_titles = [article.description for article in articles]
+    article_read = Headline.objects.get(id=news_id).description
 
-    clicked_article = Headline.objects.get(id=news_id).title
+    custom_stop_words = {'OnlineKhabar English News'}
+    all_stop_words = custom_stop_words.union(sklearn_stop_words)
+    tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, stop_words=list(all_stop_words))
+    tfidf_features = tfidf_vectorizer.fit_transform(article_titles)
 
-    vectorizer = TfidfVectorizer(stop_words='english')
-    tfidf_matrix = vectorizer.fit_transform(article_titles)
+    nmf = NMF(n_components=6)
+    nmf_features = nmf.fit_transform(tfidf_features)
+    normalized_features = normalize(nmf_features)
 
-    clicked_article_tfidf = tfidf_matrix.transform([clicked_article])
-    cosine_similarities = cosine_similarity(clicked_article_tfidf, tfidf_matrix).flatten()
-    return f'{cosine_similarities}'
+    current_article_index = article_titles.index(article_read)
+    current_article = normalized_features[current_article_index, :]
+
+    # Calculate cosine similarities
+    similarities = cosine_similarity(normalized_features, current_article.reshape(1, -1))
+
     # Get indices of similar articles (excluding the clicked article itself)
-    # https://kavita-ganesan.com/tfidftransformer-tfidfvectorizer-usage-differences/
-    similar_article_indices = cosine_similarities.argsort()[:-2:-1]
-    
+    similar_article_indices = similarities.flatten().argsort()
+
     recommended_article = article_titles[similar_article_indices[0]]
+    return f'''<h1>Article Read:</h1><h2>{article_read}</h2><br><br><h1>Article Recommend: </h1><h2>{recommended_article}</h2><br><br>
+    <h2>Similarities</h2><p>{similarities}</p>
+    <h2>Similarity index</h2><p>{similar_article_indices}</p>'''
     return f'Given Article: {article_titles[clicked_article_index]}<br><br>Recommended Article: {recommended_article}'
